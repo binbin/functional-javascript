41167f93a8d5e4f52dbfe7a8fb80236713fdcfb9
binbin <binbin5474@gmail.com>
2014-04-21 11:09:10 +0800

    recursion

--- recursion.js
+++ recursion.js
@@ -1,9 +1,16 @@
+// function reduce(arr,fn,initial){
+//   if ( arr.length == 0 ) {
+//     return initial
+//   }
+//   var result = fn(initial,arr[0])
+//   var items=arr.slice(1)
+//   return reduce(items,fn,result)
+// }
+
 function reduce(arr,fn,initial){
-  if ( arr.length == 0 ) {
-    return initial
-  }
-  var result = fn(initial,arr[0])
-  var items=arr.slice(1)
-  return reduce(items,fn,result)
+	return (function reduceOne(index,value){
+        if(index>arr.length-1) return value
+        return reduceOne(index+1,fn(value,arr[index],index,arr))
+	})(0,initial)
 }
 module.exports=reduce
